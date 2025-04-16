e99bf4974426dd06733930bea1f25a7bd9547d9d
YangTh599 <YangTh599@northwested.org>
2025-04-16 11:28:36 -0400

    did work

--- main.py
+++ main.py
@@ -15,32 +15,51 @@ def init_game():
     """Initiates Pygame, Pygame.font, and sets the Screen window and caption"""
     pygame.init()
     pygame.font.init()
+
+    ICON = pygame.image.load("sans/images/sans_head.png")
+
     pygame.display.set_caption(PYGAME_CAPTION) # Window Caption
+    pygame.display.set_icon(ICON)
 
     #Pygame Window
     window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     return window
 
 # Draw Function to update graphics
-def draw(window):
+def draw(window,chars,text):
     """DRAW FUNCTION | allows screen graphics to be added"""
     #BACKGROUND
-    window.fill(WHITE) # 15
+    window.fill(BABYBLUE) # 15
     
 
     #FOREGROUND
-    
-
+    chars.draw_image()
+    for tb in text:
+        tb.draw_text()
+ 
     #UPDATE DISPLAY
     pygame.display.update()
 
-def handle_events():
+def handle_events(sans,music):
     """Handles any pygame event such as key input"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # QUIT
             return False
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                music[1] = not music[1]
+
+                if music[1]:
+                    music[0].play(-1)
+                else:
+                    music[0].stop()
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        sans[0].rotate_image(sans[0].rotation +1)
+        
+        sans[1].play()
 
     return True
 
@@ -50,22 +69,37 @@ def main(): # MAIN FUNCTION
     clock = pygame.time.Clock()
     # ADD ALL OBJECTS/CLASSES BELOW HERE
 
+    sans = boxes.Image_box(window, 10,10,50,50, "sans/images/sansm.png")
+    sans.image = pygame.transform.scale(sans.image, (200,200))
+
+    SANS_TALK = "sans/sounds/voice_sans.mp3"
+    background_music = pygame.mixer.Sound("sans/sounds/sans..mp3")
+    music_active = False
+
+    sans_talking = pygame.mixer.Sound(SANS_TALK)
+
+    sans_stuff = [sans, sans_talking]
+    musica = [background_music, music_active]
+
+    music_text = boxes.Text_box(window, 250,25, 250,30, "Press \"p\" to play or pause background music", BLACK)
+    spin_text = boxes.Text_box(window, 250, 80, 250, 35, "Use Spacebar to spin sans the skeleton", BLACK )
     
-    
+    text = [spin_text, music_text]
+
     # ADD ALL OBJECTS/CLASSES ABOVE HERE
     run = True
     while run: # run set to true, program runs while run is true.
 
         clock.tick(FPS) # FPS Tick
 
-        run = handle_events()
+        run = handle_events(sans_stuff,musica)
         
 
         
-        draw(window) # UPDATES SCREEN
+        draw(window,sans,text) # UPDATES SCREEN
 
     pygame.quit()
-    sys.quit()
+    sys.exit()
     quit()
 # ADD CLASSES HERE
 
--- pygame_config.py
+++ pygame_config.py
@@ -1,6 +1,6 @@
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 
-PYGAME_CAPTION = "Pygame Window Caption is up here"
+PYGAME_CAPTION = "sans talking simulator"
 
 FPS = 60
 
